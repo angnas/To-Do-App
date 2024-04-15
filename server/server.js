d8808fc3ebbfb3efdd8e8b9cf415cb707345b133
Angela Naseri <f.naseri1993@gmail.com>
2024-04-15 12:35:20 +0200

    modify server.js

--- server/server.js
+++ server/server.js
@@ -1,48 +1,22 @@
 const PORT = process.env.PORT ?? 8000
 const express = require('express');
-const { v4: uuidv4 } = require('uuid')
 const cors = require('cors')
-
 const app = express();
-const pool = require('./db');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bodyParser = require("body-parser");
 
 
-app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
+const usersRoute = require("./routes/usersRoute");
+const tasksRoute = require("./routes/tasksRoute");
 
-
-
-//get all info from tasks
-
-app.get("/todos", async (req, res) => {
-    try {
-      const todos = await pool.query(
-        "SELECT * FROM todos",
-      );
-      res.json(todos.rows);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
-
-  //get all info from tasks
-
-app.get("/users", async (req, res) => {
-    try {
-      const todos = await pool.query(
-        "SELECT * FROM users",
-      );
-      res.json(todos.rows);
-    } catch (err) {
-      console.log(err);
-    }
-  });
-
+app.get("/", (req, res) => res.send("Express on Vercel with Server.js!"));
+   
+app.use("/users", usersRoute);
+app.use("/tasks", tasksRoute);
 
 
 
