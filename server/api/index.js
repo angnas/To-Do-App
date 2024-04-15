f662e21177aa5a7b1b0ea0c5e389b92b13dd881d
Angela Naseri <f.naseri1993@gmail.com>
2024-04-15 13:11:01 +0200

    modify api

---
+++ server/api/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const PORT = process.env.PORT || 3002;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const usersRoute = require('../routes/usersRoute');
+const tasksRoute = require('../routes/tasksRoute');
+
+app.get('/', (req, res) => res.send('Express on Vercel with Server.js!'));
+
+app.use('/users', usersRoute);
+app.use('/tasks', tasksRoute);
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
+
+module.exports = app;
